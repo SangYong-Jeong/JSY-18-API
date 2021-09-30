6b7d5d28e5e9ef43beb24d93ac78437b877fba30
SangYong-Jeong <mmaf@naver.com>
2021-09-30 13:21:21 +0900

    4교시 완료

--- back/routes/api/api-router.js
+++ back/routes/api/api-router.js
@@ -1,10 +1,26 @@
-const path = require('path')
 const express = require('express')
 const router = express.Router()
-// const { pool } = require('../../modules/mysql-init')
+const { pool } = require('../../modules/mysql-init')
+const jwt = require('jsonwebtoken')
 
-router.get('/', (req,res,next)=>{
-	
+router.get('/', async (req,res,next)=>{
+	let token = req.headers.authorization
+	let sql
+	try {
+		jwt.verify(token, process.env.JWT_SALT, async (err, decode)=>{
+			if(err) {
+				res.status(401).json({ err: '토큰이 만료되었습니다.'})
+			}
+			else {
+				sql = " SELECT * FROM books ORDER BY idx DESC "
+				const [books] = await pool.execute(sql)
+				res.status(200).json({ success: true, books })
+			}
+		})
+	}
+	catch(err) {
+		res.status(500).json(err)
+	}
 })
 
 module.exports = router
--- front/public/js/front.js
+++ front/public/js/front.js
@@ -1,18 +1,35 @@
 $('.bt-token').click(onGetToken)
+$('.bt-data').click(onGetData)
+
+var token = null
+let userid = 'mmaduu'
+let apikey = 'd223d69f-29a9-4b30-874e-c4ba4b362bff'
+let tokenURL = 'http://127.0.0.1:3100/api/sign'
+let dataURL = 'http://127.0.0.1:3100/api'
 
 function onGetToken () {
-	let userid = 'mmaduu'
-	let apikey = 'd223d69f-29a9-4b30-874e-c4ba4b362bff'
-	let apiURL = 'http://127.0.0.1:3100/api/sign'
 	var data = { userid: userid, apikey: apikey } 
-	axios.post(apiURL, data)
-	.then(onResult)
+	axios.post(tokenURL, data)
+	.then(getToken)
 	.catch(onError)
 
-	function onResult (r) {
+	function getToken (r) {
+		token = r.data.token
+		console.log(token)
+	}
+}
+
+function onGetData () {
+	let headers = {authorization : token}
+	axios.get(dataURL, { headers })
+	.then(getData)
+	.catch(onError)
+
+	function getData(r) {
 		console.log(r.data)
 	}
-	function onError (err) {
-		console.log(err)
-	}
+}
+
+function onError (err) {
+	console.log(err)
 }
