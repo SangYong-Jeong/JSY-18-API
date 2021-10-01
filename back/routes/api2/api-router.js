e71004367f59f681532bee4d44763677f7dc544a
SangYong-Jeong <mmaf@naver.com>
2021-10-01 10:19:37 +0900

    final

--- back/routes/api2/api-router.js
+++ back/routes/api2/api-router.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const { pool } = require('../../modules/mysql-init')
 const jwt = require('jsonwebtoken')
+const { pool } = require('../../modules/mysql-init')
 
-router.get('/', async (req,res,next)=>{
-	let sql
+router.get('/', async (req, res, next) => {
+	let sql, token
 	try {
-		res.cookie('test', '3100')
-		console.log(req.cookies)
-		if(req.cookies) {
-			res.json({ success : true, cookie: req.cookies.test })
+		if(req.cookies.token) {
+			const verifyToken = jwt.verify(req.cookies.token, process.env.JWT_SALT)
+			token = jwt.sign({ data: verifyToken.data }, process.env.JWT_SALT, { expiresIn: '10s' })
 		}
 		else {
-			res.json({ success: true, msg : '쿠키발행' })
+			sql = " SELECT * FROM users_api WHERE apikey=? AND domain=? "
+			const [rs] = await pool.execute(sql, [req.query.apikey, req.headers.origin])
+			token = jwt.sign({ data: rs[0] }, process.env.JWT_SALT, { expiresIn: '10s' })
 		}
+		res.cookie('token', token, { expires: new Date(Date.now() + 10000) }).json({ success: true, msg: '쿠키발행' })
 	}
 	catch(err) {
 		res.status(500).json(err)
